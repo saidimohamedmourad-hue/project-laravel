afc2ecd99472dcc2353acd29a2030ee1173d9afd
mohamed <saidimohamedmourad@gmail.com>
2025-12-07 10:45:57 +0000

    creat job-board1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 use App\Http\Controllers\indexcontroller;
 use App\Http\Controllers\jobcontroller;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,10 @@ Route::get('/about', [Indexcontroller::class,"about"]);
 Route::get('/contact', [Indexcontroller::class,"contact"]);
 
 route::get('job',[Jobcontroller::class,'index']);
+=======
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+>>>>>>> 7abed62 (creat job-board1)
